ca0021b54f51cb16615a36386dd9f76161502eca
Derek Alves <dev.derek.alves@gmail.com>
2024-10-01 09:09:37 -0300

    feat(core): put port to server

--- bootstrap/cmd/main.go
+++ bootstrap/cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"COREMOD/app/router"
+	"net/http"
+
+	"github.com/tabajara-coder/backend-base-structure/core"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,4 +14,8 @@ func main() {
 
 	router.InitializeMiddleware(chiRouter)
 	router.InitializeRoutes(chiRouter)
+
+	listenAddr := core.Getenv("HTTP_LISTEN_ADDR", ":8080")
+
+	http.ListenAndServe(listenAddr, chiRouter)
 }
